b0b030211b9642b4495dde8a3824715d3e052802
Taerande <revive0405@gmail.com>
2023-01-05 19:13:53 +0900

    Firt React App Expense Tracker

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
+import ExpensesList from './ExpensesList';
+
+import ExpensesChart from './ExpensesChart';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filterdExpenses = props.items.filter(v => v.date.getFullYear() === filteredYear * 1);
+  
+  
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filterdExpenses} />
+        <ExpensesList items={ filterdExpenses } />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
